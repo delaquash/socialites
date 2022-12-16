9a762e6bc5f46f5fad882822e0f35af66bfcc6aa
delaquash <olaide1191@gmail.com>
2022-12-16 10:14:50 +0100

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import bodyParser from "body-parser";
 import express from "express";
-// import mongoose from "mongoose";
 import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from "helmet";
@@ -12,11 +11,11 @@ import connectDB from "./config/connectDB.js";
 import storage from "./config/storage.js";
 
 
-// dotenv.config({ path: 'config/connectDB.js' });
-dotenv.config()
 
 // config
+dotenv.config()
 connectDB();
+
 const __fileName = fileURLToPath(
     import.meta.url);
 const __dirname = path.dirname(__fileName);
@@ -24,7 +23,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ "policy": "cross-origin" }))
+app.use(helmet.crossOriginResourcePolicy({ "policy": "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -33,9 +32,6 @@ app.use("/assets", express.static(path.join(__dirname, 'publics/assets')));
 
 // file storage
 const upload = multer({ storage });
-
-
-
 
 
 const PORT = process.env.PORT || 5000;
