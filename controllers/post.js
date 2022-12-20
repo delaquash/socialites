2ca98525325bfcf13e7b869208e2db633492547e
delaquash <olaide1191@gmail.com>
2022-12-20 16:53:00 +0100

    feat: added createPost

--- controllers/post.js
+++ controllers/post.js
@@ -1,5 +1,28 @@
 import Post from "../model/Post.js";
+import User from "../model/User.js";
 
+export const createPost = async(req, res) => {
+    try {
+        const { picturePath, userId, description } = req.params;
+        const user = await User.findById(userId);
+        const newPost = new Post({
+            userId,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            location: user.location,
+            picturePath: user.picturePath,
+            description,
+            picturePath,
+            likes: [],
+            comment: []
+        })
+        await newPost.save();
+        const post = await Post.find();
+        res.status(201).json(post)
+    } catch (error) {
+        res.status(409).json({ message: error.message })
+    }
+}
 
 export const likePost = async(req, res) => {
 
