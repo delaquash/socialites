df21ee07fd366df7074d6d68b0f9c83400535b15
delaquash <olaide1191@gmail.com>
2022-12-20 14:01:39 +0100

    feat: add addRemoveFriend

--- controllers/users.js
+++ controllers/users.js
@@ -31,3 +31,36 @@ export const getUserFriends = async(req, res) => {
         res.status(404).json({ error: error.message })
     }
 }
+
+
+export const addRemovefriend = async(req, res) => {
+    try {
+        const { id, friendId } = req.params;
+        const user = await User.findById(id);
+        const friend = await User.findById(friendId);
+
+        if (user.friends.includes(friendId)) {
+            user.friends = user.friends.filter((id) => id !== friendId);
+            friend.friends = friend.friends.filter((id) => id !== id);
+
+        } else {
+            user.friends.push(friendId);
+            friend.friends.push(id)
+        }
+        await user.save();
+        await friend.save()
+
+        const friends = await Promise.all(
+            user.friends.map((id) => User.findById(id))
+        );
+        /* Destructuring the object and returning a new object with the same properties. */
+        const formatedFriends = friends.map(
+            ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+                return { _id, firstName, lastName, occupation, location, picturePath }
+            }
+        )
+        res.status(200).json(formatedFriends)
+    } catch (error) {
+        res.status(404).json({ error: error.message })
+    }
+}
