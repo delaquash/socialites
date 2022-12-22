6f58b65e57c6d4ea8f8a838d5c361a9b8b6347cf
delaquash <olaide1191@gmail.com>
2022-12-22 15:37:09 +0100

    feat: add page router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import NavBar from './pages/NavBar';
+import ProfilePage from './pages/ProfilePage';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Routes>
+      <Route path='/' element={<LoginPage />} />
+      <Route path='/home' element={<HomePage />} />
+      <Route path='/profile/:userId' element={<ProfilePage />} />
+    </Routes>
   )
 }
 
