f78cbbaaa9d53cb71ca00449ba8cef069bb961f0
delaquash <olaide1191@gmail.com>
2022-12-23 12:10:28 +0100

    feat: redux state for auth

--- client/src/state/index.tsx
+++ client/src/state/index.tsx
@@ -1,1 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+interface State {
+    mode: string,
+    user: string | null,
+    token: string | null,
+    post: []
+}
+
+const initialState:State= {
+    mode: "light",
+    user: null,
+    token: null,
+    post: []
+
+}
+
+export const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        setMode: (state) => {
+            state.mode = state.mode === "light" ? "dark" : "light";
+        },
+        setLogin: (state, action) => {
+            state.user = action.payload.user;
+            state.token = action.payload.token
+        },
+        setLogout: (state, action) => {
+            state.user = null;
+            state.token = null; 
+        }
+    }
+})
