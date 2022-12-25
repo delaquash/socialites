8866aac4223f5f8aebde592b763525c0ab8299e4
delaquash <olaide1191@gmail.com>
2022-12-25 16:26:26 +0100

    feat: add redux persist

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import authReducers from './state/index';
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 import { BrowserRouter } from "react-router-dom";
+import { persistStore, FLUSH, persistReducer,PERSIST, PAUSE, REHYDRATE, PURGE, REGISTER } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+
+
+const persistConfig = { key: "root", storage, version: 1};
+const persistedReducer =persistReducer(persistConfig, authReducers);
+const store = configureStore({
+  reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => 
+     getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, REGISTER, PAUSE, PERSIST, PURGE]
+      }
+     }) 
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
       <React.StrictMode>
-        <App />
+        <Provider store={store}>
+          <PersistGate persistor={persistStore(store)} loading={null}>
+            <App />
+          </PersistGate>
+        </Provider>
       </React.StrictMode>,
   </BrowserRouter>
 )
