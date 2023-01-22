6d0c0b9d78784d3f1e221e041056b39077a5e3cf
delaquash <olaide1191@gmail.com>
2023-01-22 19:27:14 +0100

    feat: add textfiled to form component

--- clients/src/Pages/LoginPage/Form.jsx
+++ clients/src/Pages/LoginPage/Form.jsx
@@ -17,17 +17,17 @@ import Dropzone from "react-dropzone";
 import FlexBetween from "components/FlexBetween";
 
 const registerSchema = yup.object().shape({
-    firstName: yup.string().required("required"),
-    lastName: yup.string().required("required"),
-    password: yup.string().email("invalid email").required("required"),
-    email:yup.string().required("required"),
-    location: yup.string().required("required"),
-    occupation: yup.string().required("required"),
+    firstName: yup.string().required("Firstname is required").min(2, "Too Short!").max(50, "Too Long!"),
+    lastName: yup.string().required("Lastname is required").min(2, "Too Short!").max(50, "Too Long!"),
+    password: yup.string().required("Invalid password").required("Password is required"),
+    email:yup.string().required("Email is required").min(2, "Too Short!").max(50, "Too Long!"),
+    location: yup.string().required("Location is required").min(2, "Too Short!").max(50, "Too Long!"),
+    occupation: yup.string().required("Occupation is required").min(2, "Too Short!").max(50, "Too Long!"),
 });
 
 const loginSchema = yup.object().shape({
-    email: yup.string().email("invalid email").required("required"),
-    password: yup.string().required("required"),
+    email: yup.string().email("invalid email").required("Email is required").min(2, "Too Short!").max(50, "Too Long!"),
+    password: yup.string().required("Password is required").min(2, "Too Short!").max(50, "Too Long!"),
   });
   
   const initialValuesRegister = {
@@ -54,7 +54,7 @@ const Form = () => {
   const isLogin = pageType === "login";
   const isRegister = pageType === "register"
 
-  const handleSubmit=(e)=> {
+  const handleSubmit=(e, values, onSubmitProps)=> {
     e.preventDefault()
   }
   return (
@@ -63,7 +63,94 @@ const Form = () => {
       initialValues={isLogin ? initialValuesLogin : initialValuesRegister}
       validationSchema={isLogin ? loginSchema : registerSchema }
     >
+      {/* (({values, 
+         errors, 
+         touched, 
+         handleBlur, 
+         handleChange, 
+         handleSubmit, 
+         setFieldValue, 
+         resetForm})
+         => ( */}
+        {(formik) => {
+        const {
+          values,
+          handleChange,
+          handleSubmit,
+          errors,
+          touched,
+          handleBlur,
+          isValid,
+          dirty
+        } = formik;
+        <Form onSubmit={handleSubmit}>
+          <Box
+              display="grid"
+              gap="30px"
+              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+              sx={{
+                "& > div": {gridColumn: isNonMobile ? undefined : "span 4"}
+              }}
+          >
+            {isRegister && (
+              <>
+              <TextField
+                  label="First Name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  values={values.firstName}
+                  name="firstName"
+                  errors={Boolean(touched.firstName) && Boolean(errors.firstName)}
+                  helperText={touched.firstName && errors.firstName}
+                  sx={{ gridColumn: "span 2"}}
+              />
+              <TextField
+                  label="Last Name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  values={values.lastName}
+                  name="lastName"
+                  errors={Boolean(touched.lastName) && Boolean(errors.lastName)}
+                  helperText={touched.lastName && errors.lastName}
+                  sx={{ gridColumn: "span 2"}}
+              />
+              <TextField
+                  label="Location"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  values={values.location}
+                  name="location"
+                  errors={Boolean(touched.location) && Boolean(errors.location)}
+                  helperText={touched.location && errors.location}
+                  sx={{ gridColumn: "span 2"}}
+              />
+               <TextField
+                  label="Occupation"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  values={values.occupation}
+                  name="occupation"
+                  errors={Boolean(touched.occupation) && Boolean(errors.occupation)}
+                  helperText={touched.occupation && errors.occupation}
+                  sx={{ gridColumn: "span 2"}}
+              />
+                <Box
+                  gridColumn="span 4"
+                  border={`1px solid ${palette.neutral.medium}`}
+                  borderRadius="5px"
+                  p="1rem"
+                >
+                  <Dropzone
+                      acceptedFiles=".jpg, .jpeg, .png"
+                  >
 
+                  </Dropzone>
+                </Box>
+              </>
+            )}
+          </Box>
+        </Form>
+        }}
     </Formik>
   )
 }
