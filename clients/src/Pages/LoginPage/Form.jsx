c008a4da680209609f56ed4f03a99953a336a047
delaquash <olaide1191@gmail.com>
2023-01-22 19:40:36 +0100

    feat: add dropzone for image upload

--- clients/src/Pages/LoginPage/Form.jsx
+++ clients/src/Pages/LoginPage/Form.jsx
@@ -142,8 +142,29 @@ const Form = () => {
                 >
                   <Dropzone
                       acceptedFiles=".jpg, .jpeg, .png"
+                      multiple={false}
+                      onDrop={(acceptedFiles) => 
+                        setFieldValue("picture", acceptedFiles[0])
+                      }
                   >
-
+                      {({ getRootProps, getInputProps }) => (
+                        <Box
+                            {...getRootProps()}
+                            border={`2px dashed ${palette.primary.main}`}
+                            p="1rem"
+                            sx={{ "$:hover": { cursor: "pointer" }}}
+                        >
+                          <input {...getInputProps()}/>
+                          {!values.picture ? (
+                            <p>Add pictures here...</p>
+                          ): (
+                            <FlexBetween>
+                              <Typography>{values.picture.name}</Typography>
+                              <EditOutlinedIcon />
+                            </FlexBetween>
+                          )}
+                        </Box>
+                      )}
                   </Dropzone>
                 </Box>
               </>
